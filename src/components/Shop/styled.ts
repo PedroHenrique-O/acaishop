7c75cb959d8334c8e955b424f40fe09f6d8eeadc
Pedro <pedrokaz.henrique@gmail.com>
2022-01-03 11:44:41 -0300

    shop styles

---
+++ src/components/Shop/styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 3rem;
+  justify-content: center;
+  align-items: center;
+`;
