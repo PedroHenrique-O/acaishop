5e93bd8c9987e3f7513c650ec368a2d5f3a1b133
Pedro <pedrokaz.henrique@gmail.com>
2022-01-03 11:45:50 -0300

    added header

---
+++ src/components/Header/styled.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: 700;
+  font-size: 1.3rem;
+  margin: 2rem 3rem;
+  padding: 0 5.5rem;
+
+  h1 {
+    span {
+      color: purple;
+    }
+  }
+
+  .cartButton {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    span {
+      display: flex;
+      justify-content: center;
+      align-content: center;
+      position: absolute;
+      right: 40px;
+      background-color: purple;
+      border-radius: 1rem;
+      padding: 0.1rem 0.7rem;
+      width: 1.5rem;
+    }
+  }
+`;
