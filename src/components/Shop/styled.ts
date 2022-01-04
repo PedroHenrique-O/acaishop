497c0d61471dc3f77616e794c10725a6ea7e3d05
Pedro <pedrokaz.henrique@gmail.com>
2022-01-04 19:14:17 -0300

    shop styling

--- src/components/Shop/styled.ts
+++ src/components/Shop/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
-  padding: 3rem;
+
   justify-content: center;
   align-items: center;
 `;
