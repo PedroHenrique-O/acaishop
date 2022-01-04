347631961669e1a22f5c402cfa70b4f7a8fd54db
Pedro <pedrokaz.henrique@gmail.com>
2022-01-04 19:15:05 -0300

    added route navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,25 @@
 import { Container } from "./styled";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useAppSelector } from "../../reducers/hooks";
-
+import { Link } from "react-router-dom";
 export function Header() {
-  const cart = useAppSelector((state) => state.cart.products);
+  const cart = useAppSelector((state) => state.cart.products.length);
+
+  <Link to="/cart" />;
+
   return (
     <Container>
-      <h1>
-        Açai <span>+</span>
-      </h1>
-
-      <div className="cartButton">
-        <span> </span>
-        <AiOutlineShoppingCart size="2.5rem" />
-      </div>
+      <Link to="/">
+        <h1>
+          Açai <span>+</span>
+        </h1>
+      </Link>
+      <Link to="/cart">
+        <div className="cartButton">
+          <span> {cart}</span>
+          <AiOutlineShoppingCart size="2.5rem" />
+        </div>
+      </Link>
     </Container>
   );
 }
