56435a1d3e8bf4d18e2fa61bbf5121c706d27c44
Pedro <pedrokaz.henrique@gmail.com>
2022-01-05 15:33:20 -0300

    cart styling

--- src/components/Cart/styled.ts
+++ src/components/Cart/styled.ts
@@ -1,3 +1,4 @@
+import { darken } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -14,32 +15,46 @@ export const Container = styled.div`
     thead th {
       text-align: left;
       padding-left: 2rem;
+      color: #666;
     }
 
     tbody td {
       text-align: left;
       padding-left: 2rem;
+      border-bottom: 1px solid #eee;
 
       div {
         display: flex;
         align-items: center;
         font-weight: 700;
+        gap: 1rem;
 
         span {
           svg {
-            fill: purple;
+            color: #000;
+
+            &:hover {
+              fill: ${darken(0.1, "#94187d")};
+            }
             cursor: pointer;
             margin-left: 0.5rem;
           }
         }
+      }
+      .deleteBtn {
+        svg {
+          color: #000;
+          cursor: pointer;
 
-        gap: 1rem;
+          &:hover {
+            fill: ${darken(0.1, "#94187d")};
+          }
+        }
       }
 
       img {
-        width: 250px;
-        height: 130px;
-
+        width: 130px;
+        height: 80px;
         border-radius: 10px;
       }
       p {
@@ -68,6 +83,22 @@ export const Container = styled.div`
       }
     }
     .amountWrapp {
+    }
+  }
+
+  footer {
+    display: flex;
+    width: 100%;
+    justify-content: flex-end;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 1rem;
+
+    .cartTotal {
+      margin-top: 0.3rem;
+      strong {
+        display: block;
+      }
     }
   }
 `;
