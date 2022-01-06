93c8973975657d54c653c9688333c8f582cf6052
Pedro <pedrokaz.henrique@gmail.com>
2022-01-06 16:47:45 -0300

    add loading comp.

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -3,7 +3,10 @@ import { selectCount } from "../../reducers/CartSlice";
 import { useAppSelector } from "../../reducers/hooks";
 import { api } from "../../services/api";
 import { AcaiItem } from "../AcaiItem";
+import { Header } from "../Header";
 import { Container } from "./styled";
+import ReactLoading from "react-loading";
+import { ToastContainer } from "react-toastify";
 
 interface Products {
   _id: string;
@@ -17,23 +20,33 @@ interface Products {
 export function Shop() {
   const [products, setProducts] = useState<Products[]>([]);
   const cart = useAppSelector(selectCount);
+  const [isLoading, setIsLoading] = useState(false);
   console.log(cart.products);
   console.log("Products: ", products);
 
   const getProducts = async () => {
+    setIsLoading(true);
     const req = await api.get("/products.json");
     const data = await req.data;
+
     setProducts(data);
+    setIsLoading(false);
   };
   useEffect(() => {
     getProducts();
   }, []);
 
   return (
-    <Container>
-      {products.map((item) => (
-        <AcaiItem key={item._id} {...item} />
-      ))}
-    </Container>
+    <>
+      <Header />
+      <Container>
+        {isLoading && (
+          <ReactLoading type="spin" color="#fff" height={"10%"} width={"10%"} />
+        )}
+        {products.map((item) => (
+          <AcaiItem key={item._id} {...item} />
+        ))}
+      </Container>
+    </>
   );
 }
