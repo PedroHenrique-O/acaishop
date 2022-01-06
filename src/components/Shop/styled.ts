f7b86d53c67892cfdfba8357f2db4da10e9fda7f
Pedro <pedrokaz.henrique@gmail.com>
2022-01-06 16:47:16 -0300

    shop styling

--- src/components/Shop/styled.ts
+++ src/components/Shop/styled.ts
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
-
   justify-content: center;
   align-items: center;
+  margin-top: 4rem;
+  height: auto;
 `;
